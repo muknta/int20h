dbde24d1fef3a10c946464634d1e0eeb4e3ec3be
NKostetskiy <santax@ukr.net>
2020-01-19 13:33:42 +0200

    Initial commit

---
+++ app/route_handlers.py
@@ -0,0 +1,12 @@
+
+
+def recognize_song_by_text(request):
+    return 'by text', 200
+
+
+def recognize_song_by_voice(request):
+    return 'by_voice', 200
+
+
+def recognize_song_by_sound(request):
+    return 'by_sound', 200
---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from app import app
+from flask import render_template, jsonify, request
+from config import API
+from .route_handlers import *
+
+
+@app.route('/')
+@app.route('/int20h')
+def init():
+    return render_template('init_page.html')
+
+
+@app.route(f'{API}/song/recognize', methods=['POST'])
+def recognize_song():
+    data = request.get_json()
+    print(data)
+    recognition_type = data.get('type')
+    if recognition_type is not None:
+        if recognition_type == 'text':
+            return recognize_song_by_text(data)
+        elif recognition_type == 'voice':
+            return recognize_song_by_voice(data)
+        elif recognition_type == 'sound':
+            return recognize_song_by_sound(data)
+    return jsonify(msg='Invalid data', result=False), 400
