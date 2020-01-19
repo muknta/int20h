f61fbcde679503ef69c59ec5902243fd7a21c53c
NKostetskiy <santax@ukr.net>
2020-01-19 14:21:03 +0200

    Add WSGIServer

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
+from gevent.pywsgi import WSGIServer
 from app import app
+from gevent import monkey; monkey.patch_all()
+
+monkey.patch_all()
+app.debug = True
+
+
+"Start gevent WSGI server"
+# use gevent WSGI server instead of the Flask
+http = WSGIServer(('127.0.0.1', 5000), app)
+# TODO gracefully handle shutdown
+http.serve_forever()
+
