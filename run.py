80f67f69003d583f949d63c8825cccc94886d6cc
NKostetskiy <santax@ukr.net>
2020-01-19 14:28:57 +0200

    Small refactor

--- run.py
+++ run.py
@@ -2,13 +2,14 @@ from gevent.pywsgi import WSGIServer
 from app import app
 from gevent import monkey; monkey.patch_all()
 
-monkey.patch_all()
 app.debug = True
 
 
-"Start gevent WSGI server"
-# use gevent WSGI server instead of the Flask
-http = WSGIServer(('127.0.0.1', 5000), app)
-# TODO gracefully handle shutdown
-http.serve_forever()
+def main():
+    monkey.patch_all()
+    http = WSGIServer(('127.0.0.1', 5000), app)
+    http.serve_forever()
 
+
+if __name__ == '__main__':
+    main()
