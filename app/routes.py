92c2b51fe30ba37f6e8e227d6853c55ccc49616a
Dmytro <dzelenyi9@gmail.com>
2020-01-21 22:25:49 +0200

    uploading a file

--- app/routes.py
+++ app/routes.py
@@ -3,18 +3,38 @@ from flask import render_template, jsonify, request
 from config import API
 from .route_handlers import *
 
-@app.route('/send')
-def sent_song():
-    return render_template("upload_file.html")
+import os
+from flask import flash, redirect, url_for
+from werkzeug.utils import secure_filename
 
-@app.route('/getsong', methods=['GET', 'POST'])
-def get_part_song(request):
-    print("Heloo!!!!!!!!!!---------------------")
+
+UPLOAD_FOLDER = '/home/dmytro/files'
+ALLOWED_EXTENSIONS = {'mp3', 'ogg'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
     if request.method == 'POST':
-        data = request.form['myfile']
-        return render_template('init_page.html')
-    
-    return render_template('init_page.html')
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template("init_page.html", text="SUCCESSFULL")
 
 
 @app.route('/')
