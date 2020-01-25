a035df708a4fc77a9a6f5af5ecd4d8db4d284f9f
NKostetskiy <santax@ukr.net>
2020-01-25 20:24:17 +0200

    Try to build:

--- run.py
+++ run.py
@@ -1,19 +1,9 @@
 from gevent import monkey
-from gevent.pywsgi import WSGIServer
 
 monkey.patch_all()
 from app import app
-import os
 
 app.debug = True
 
-
-def main():
-    # app.run('0.0.0.0', 5000)
-    port = int(os.environ.get('PORT', 5000))
-    http = WSGIServer(('0.0.0.0', port), app)
-    http.serve_forever()
-
-
 if __name__ == '__main__':
-    main()
+    app.run()
