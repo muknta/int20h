2acf8b5834fc474a8d2035acbfeec208fd5511a6
NKostetskiy <santax@ukr.net>
2020-01-25 20:16:36 +0200

    Try to deploy

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn --bind 0.0.0.0:${PORT} wsgi
+web: python run.py
 
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from gevent import monkey
+from gevent.pywsgi import WSGIServer
+
 monkey.patch_all()
 from app import app
 import os
@@ -8,10 +10,9 @@ app.debug = True
 
 def main():
     # app.run('0.0.0.0', 5000)
-    # # http = WSGIServer(('127.0.0.1', 5000), app)
-    # # http.serve_forever()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    http = WSGIServer(('0.0.0.0', port), app)
+    http.serve_forever()
 
 
 if __name__ == '__main__':
