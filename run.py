1c5ee7c36bd1ae7b33cb2d3871c0d328483c126c
NKostetskiy <santax@ukr.net>
2020-01-25 20:43:12 +0200

    Try to build

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from gevent import monkey
 import os
 monkey.patch_all()
-from app import app
+from app import as application
 
-app.debug = True
+application.debug = True
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    application.run(host='0.0.0.0', port=port)
