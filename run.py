a57b3ab17347e35b5e8363722760bdc80e6b0b13
NKostetskiy <santax@ukr.net>
2020-01-25 19:59:30 +0200

    Try to deploy

--- run.py
+++ run.py
@@ -1,16 +1,15 @@
 from gevent import monkey
 monkey.patch_all()
-
-from gevent.pywsgi import WSGIServer
 from app import app
 
 app.debug = True
 
 
 def main():
-    http = WSGIServer(('127.0.0.1', 5000), app)
-    http.serve_forever()
-    #app.run(host='0.0.0.0')
+    # app.run('0.0.0.0', 5000)
+    # # http = WSGIServer(('127.0.0.1', 5000), app)
+    # # http.serve_forever()
+    app.run(host='0.0.0.0', port=5000)
 
 
 if __name__ == '__main__':
